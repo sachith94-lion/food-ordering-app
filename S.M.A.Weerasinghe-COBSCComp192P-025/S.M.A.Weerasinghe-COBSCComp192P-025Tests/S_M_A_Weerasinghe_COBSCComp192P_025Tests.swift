c8097c4f192c43fb6644185bfbd7c2fa3b1cf228
sachith94-lion <sachithmanuradha@gmail.com>
2021-03-06 09:29:30 +0530

    init project

---
+++ S.M.A.Weerasinghe-COBSCComp192P-025/S.M.A.Weerasinghe-COBSCComp192P-025Tests/S_M_A_Weerasinghe_COBSCComp192P_025Tests.swift
@@ -0,0 +1,33 @@
+//
+//  S_M_A_Weerasinghe_COBSCComp192P_025Tests.swift
+//  S.M.A.Weerasinghe-COBSCComp192P-025Tests
+//
+//  Created by Lionel Rajapakse on 2021-03-06.
+//
+
+import XCTest
+@testable import S_M_A_Weerasinghe_COBSCComp192P_025
+
+class S_M_A_Weerasinghe_COBSCComp192P_025Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
